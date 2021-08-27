dd4d676d4def45ef9d881c25612f4a6660f5da46
Seongho Jin <40506736+Wordbe@users.noreply.github.com>
2021-08-27 23:10:20 +0900

    feat: sleuth + zipkin 을 통해 마이크로서비스 간 통신 로그 추적, 의존관계 파악

--- order-service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -8,17 +8,17 @@ import com.example.orderservice.service.OrderService;
 import com.example.orderservice.util.ModelMapperUtils;
 import com.example.orderservice.vo.RequestOrder;
 import com.example.orderservice.vo.ResponseOrder;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.UUID;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/order-service")
 @RestController
@@ -42,6 +42,8 @@ public class OrderController {
         OrderDto orderDto =  modelMapperUtils.mapper().map(orderDetails, OrderDto.class);
         orderDto.setUserId(userId);
 
+        log.info("[order service] order data 추가하기 전...");
+
         // JPA 로 주문 생성
         OrderDto createdOrder = orderService.createOrder(orderDto);
         ResponseOrder responseOrder = modelMapperUtils.mapper().map(createdOrder, ResponseOrder.class);
@@ -54,13 +56,24 @@ public class OrderController {
 
 //        kafkaProducer.send("catalog-topic", orderDto);
 
+        log.info("[order service] order data 추가되었음");
         return ResponseEntity.status(HttpStatus.CREATED).body(responseOrder);
     }
 
     @GetMapping("/{userId}/orders")
     public ResponseEntity<List<ResponseOrder>> getOrders(
             @PathVariable("userId") String userId) {
+        log.info("[order service] order data 받기 전...");
         List<OrderEntity> orders = (List<OrderEntity>) orderService.getOrdersByUserId(userId);
+
+        try {
+            Thread.sleep(1000);
+            throw new RuntimeException("장애 발생");
+        } catch (InterruptedException e) {
+            log.warn(e.getMessage());
+        }
+
+        log.info("[order service] order data 받았음");
         return ResponseEntity.status(HttpStatus.OK).body(
                 orders.stream()
                 .map(orderEntity -> modelMapperUtils.mapper().map(orderEntity, ResponseOrder.class))
